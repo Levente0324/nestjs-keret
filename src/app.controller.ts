f98d8f0456c71df2b29a2ebfb525b93ec7b64371
Petrik Diak <diak@petrik.hu>
2024-09-20 16:42:23 +0200

    first commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,35 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Param, Delete, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/quotes')
   @Render('index')
-  getHello() {
+  findAll() {
     return {
-      message: this.appService.getHello(),
-    };
+      quotes: this.appService.findAll()
+    }
+  }
+
+  @Get('quote/:id')
+  oneQuote(@Param('id') id: string) {
+    return this.appService.oneQuote(+id);
+  }
+
+  @Delete('deleteQuote/:id')
+  deleteQuote(@Param('id') id: string) {
+    return this.appService.deleteQuote(+id);
+  }
+
+  @Get('randomQuote')
+  randomQuote() {
+    return this.appService.randomQuote();
+  }
+
+  @Get('topAuthor')
+  topAuthor() {
+    return this.appService.topAuthor();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,47 @@
 import { Injectable } from '@nestjs/common';
+import {tsquotes}  from "./quotes.js"
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+    private quotes = tsquotes;
+  
+    //
+  
+    findAll() {
+      return this.quotes;
+    }
+  
+    //
+  
+    randomQuote() {
+      return this.quotes[Math.floor(Math.random() * this.quotes.length)].quote;
+    }
+  
+    //
+  
+    topAuthor() {
+      const authorMap = new Map<string, number>();
+  
+      this.quotes.forEach((quote) => {
+        authorMap.set(quote.author, (authorMap.get(quote.author) || 0) + 1);
+      });
+  
+      return Array.from(authorMap);
+    }
+  
+    //
+  
+    oneQuote(id: number) {
+      const user = this.quotes.find((quote) => quote.id === id);
+  
+      return user;
+    }
+  
+    //
+  
+    deleteQuote(id: number) {
+      const removedUser = this.oneQuote(id);
+  
+      return removedUser;
+    }
   }
-}
